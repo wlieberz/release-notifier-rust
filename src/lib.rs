41b933a6bb44244c9c8887ee537bc1549b31d169
William Lieberz <williamlieberz@gmail.com>
2022-09-05 17:10:37 -0700

    Add unit-test changelog_with_v

--- src/lib.rs
+++ src/lib.rs
@@ -138,4 +138,37 @@ a valid header for each version.
 
         get_latest_changelog_entry(&changelog_content).unwrap();
     }
+
+#[test]
+fn test_changelog_with_v() {
+    let changelog_content = "\
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## [v1.2.26] - 2022-06-24
+- Many bugfixes
+- Much more stable. Use this version.
+
+## [v1.0.0] - 2022-06-16
+- Major breaking changes.
+- Much better now.
+- Many new features.
+
+## [v0.1.0] - 2022-06-15
+- Initial release.";
+
+    let expected = "\
+## [v1.2.26] - 2022-06-24
+- Many bugfixes
+- Much more stable. Use this version.
+
+";
+    let result = get_latest_changelog_entry(&changelog_content);
+
+    assert_eq!(expected, result.unwrap());
+}
+
 }
